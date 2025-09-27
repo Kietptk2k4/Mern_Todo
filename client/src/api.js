354803132ae50e04e1bfa26177eec3251ef6ff87
tuan kiet <kietpt301104@gmail.com>
2025-09-28 00:14:12 +0700

    change in api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,5 +1,7 @@
+// client/src/api.js
 import axios from "axios";
 
+// Lấy API_BASE từ file .env (Vercel/Netlify sẽ inject biến này)
 const API_BASE = import.meta.env.VITE_API_BASE;
 
 const api = axios.create({
